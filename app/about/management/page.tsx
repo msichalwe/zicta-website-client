f83d3819eb0bf888fbf864754fc7fa9031805c23
Kaputo Kalandanya <kaputokalan11@gmail.com>
2023-08-31 16:31:45 +0200

    management page

--- app/about/management/page.tsx
+++ app/about/management/page.tsx
@@ -3,8 +3,23 @@ import Footer from '@/app/components/footer'
 import Navbar from '@/app/components/navbar'
 import React from 'react'
 import { Balancer } from 'react-wrap-balancer'
+import useSWR from 'swr'
+import { fetcher } from '@/lib/fetcher'
+import Loader from '@/components/ui/loader'
 
 const Management = () => {
+	const { data, isLoading } = useSWR(
+		`${process.env.NEXT_PUBLIC_API_URL}/about/management`,
+		fetcher,
+	)
+
+	if (isLoading) {
+		return (
+			<div>
+				<Loader />
+			</div>
+		)
+	}
 	return (
 		<>
 			<Navbar />
@@ -18,7 +33,7 @@ const Management = () => {
 					</p>
 				</div>
 				<div className="min-h-screen mx-auto w-5/6">
-					<div className="w-3/5 space-y-5">
+					<div className="md:w-3/5 w-full space-y-5">
 						<h2 className="text-4xl  bg-gradient-to-r from-zicta-blue  to-blue-400 bg-clip-text text-transparent font-bold">
 							Meet the Team of Directors
 						</h2>
@@ -29,6 +44,31 @@ const Management = () => {
 							</Balancer>
 						</p>
 					</div>
+
+					<ul
+						role="list"
+						className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+						{data.map((person: any) => (
+							<li
+								key={person._id}
+								className={` ${
+									person.description === 'Director General' ? 'col-span-3 ' : ''
+								} flex flex-col items-center`}>
+								<img
+									className="h-40 w-40 rounded-2xl object-cover border-2 border-zicta-yellow "
+									src={person.imageUrl}
+									alt=""
+								/>
+								<h3 className="mt-6 text-base font-semibold text-center text-gray-900">
+									{person.name}
+								</h3>
+
+								<p className="text-base leading-7 text-center text-gray-600">
+									{person.description}
+								</p>
+							</li>
+						))}
+					</ul>
 				</div>
 			</div>
 			<Footer />
